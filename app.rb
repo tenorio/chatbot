1bdc13bdc6afa0d7bdc22cfd31199ef1020ef96e
Marcelo Tenorio de Souza <marcelllo.martin@gmail.com>
2018-03-24 22:29:25 -0300

    Add models

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
-
 require './config/database'
+
+Dir['./app/models/*.rb'].each do |file|
+  require file
+end
 
 class App < Sinatra::Base
   get '/sinatra' do
